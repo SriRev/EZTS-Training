8e9791021b67962380a0fb142f82f7fcf1abf288
SriRev <srinivasshanbogh@gmail.com>
2024-06-24 19:51:38 +0530

    Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,3 +1,18 @@
+# Method 1
+
+n=int(input("enter no:"))
+f=1
+if n==0:
+    print("1")
+else:
+    for i in range(1,n+1):
+        f=f*i
+    print(f)
+
+
+
+# Method 2
+
 def fact(n):
     if n==0:
         return 0
