22e642b0c4ca75f0b4387ee631b3cf1a821cfb48
Srinivas S <srinivasshanbogh@gmail.com>
2024-06-27 11:07:27 +0530

    Add files via upload

---
+++ Lambda_function.py
@@ -0,0 +1,12 @@
+def a(x):
+    return x-4
+b=lambda x:x-4
+print(a(3),end=' ')
+print(b(3))
+
+
+def a(x,y,z):
+    return x*2+y*3+z*4
+b=lambda x,y,z:x*2+y*3+z*4
+print(a(2,3,4),end=' ')
+print(b(2,3,4))
