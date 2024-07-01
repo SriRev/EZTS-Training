6ab64c74d474544f1bab918bc9ec3ed077c36a7b
Srinivas S <srinivasshanbogh@gmail.com>
2024-07-01 10:45:18 +0530

    Knapsack_profit_weight.py

--- Knapsack_profit_weight.py
+++ Knapsack_profit_weight.py
@@ -27,39 +27,39 @@ print(spw)
 
 # method 2---->Recursion
 
-# def cal_max(p,w,c,n):
-#     if n==0 or c==0:
-#         return 0
-#     if w[n-1]>c:
-#         return cal_max(p,w,c,n-1)
-#     else:
-#         return max(p[n-1]+cal_max(p,w,c-w[n-1],n-1),cal_max(p,w,c,n-1))
+def cal_max(p,w,c,n):
+    if n==0 or c==0:
+        return 0
+    if w[n-1]>c:
+        return cal_max(p,w,c,n-1)
+    else:
+        return max(p[n-1]+cal_max(p,w,c-w[n-1],n-1),cal_max(p,w,c,n-1))
 
 
-# p=[5,10,15,7,8,9,4]
-# w=[1,3,5,4,1,3,2]
-# c=15
-# n=len(p)
-# print(cal_max(p,w,c,n))
+p=[5,10,15,7,8,9,4]
+w=[1,3,5,4,1,3,2]
+c=15
+n=len(p)
+print(cal_max(p,w,c,n))
 
 # Method 3---->Memorization
 
-# def cal_max(p,w,c,n):
-#     if n==0 or c==0:
-#         return 0
-#     if dp[n][c]!=0:
-#         return dp[n][c]
-#     if w[n-1]<=c:
-#         dp[n][c]=max(p[n-1]+cal_max(p,w,c-w[n-1],n-1),cal_max(p,w,c,n-1))
-#         return dp[n][c]
-#     else:
-#         dp[n][c]=cal_max(p,w,c,n-1)
-#         return dp[n][c]
+def cal_max(p,w,c,n):
+    if n==0 or c==0:
+        return 0
+    if dp[n][c]!=0:
+        return dp[n][c]
+    if w[n-1]<=c:
+        dp[n][c]=max(p[n-1]+cal_max(p,w,c-w[n-1],n-1),cal_max(p,w,c,n-1))
+        return dp[n][c]
+    else:
+        dp[n][c]=cal_max(p,w,c,n-1)
+        return dp[n][c]
 
-# p=[5,10,15,7,8,9,4]
-# w=[1,3,5,4,1,3,2]
-# c=max(p)
-# n=len(p)
-# dp=[[0 for i in range(c+1)] for j in range(n+1)]
-# print(cal_max(p,w,c,n))
-# print(dp)
+p=[5,10,15,7,8,9,4]
+w=[1,3,5,4,1,3,2]
+c=max(p)
+n=len(p)
+dp=[[0 for i in range(c+1)] for j in range(n+1)]
+print(cal_max(p,w,c,n))
+print(dp)
